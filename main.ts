fb28434cc4c071a94a8cd026891da37966c052aa
Kaleid0sc0pe <k.linder@mcaabhm.org>
2024-10-04 12:24:59 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
-let GameNumber = randint(1, 10)
-let GuessNumber = game.askForNumber("Guess a number 1-10")
-if (GameNumber == GuessNumber) {
-    game.splash("Great Guess!")
-} else {
-    game.splash("No it was " + GameNumber)
-}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    answer = randint(1, 4)
+    if (answer == 1) {
+        game.splash("Signs look good")
+    } else if (answer == 2) {
+        game.splash("Could be")
+    } else if (answer == 3) {
+        game.splash("Not very likely")
+    } else {
+        game.splash("Probably mot ")
+    }
+})
+let answer = 0
+game.splash("Think of a question", "then press A")
